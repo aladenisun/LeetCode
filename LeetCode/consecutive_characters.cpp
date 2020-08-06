0ecdec6ef051e24d7ec38cc0f6459e4795647ddd
nisunalade <nisunalade13@gmail.com>
2020-08-06 00:41:46 -0700

    consecutiveCharacters

--- LeetCode/consecutive_characters.cpp
+++ LeetCode/consecutive_characters.cpp
@@ -16,7 +16,7 @@ public:
             
             l e e t c o d e
         */
-      
+        if (s.size() == 0) return 0;
         for(current=1; current<s.size(); current++){
             printf("current = %i\n", current);
             if (s[current] == s[current-1] ) freq++; // freq = 2

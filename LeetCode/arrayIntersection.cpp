0fd8ec9e6cd61ae849ec2516a72d66f87de7087d
nisunalade <nisunalade13@gmail.com>
2020-08-09 15:10:51 -0700

    Intersection

---
+++ LeetCode/arrayIntersection.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+       /*
+       - sort the arrays and create i&j indexes
+       - while(i != size1 and i != size2){
+            - if nums1[i] == nums2[j] then result.push_back(nums1[i])
+            -else if (nums1[i] < nums2[j]) then i++;
+            -else j++;
+       }
+       */
+       
+       sort(nums1.begin(), nums1.end());
+       sort(nums2.begin(), nums2.end());
+       
+       int i = 0;
+       int j = 0;
+       
+       vector<int> result;
+       
+       while(i != nums1.size() || j != nums2.size()){
+           if(nums1[i] == nums2[j]){
+               result.push_back(nums1[i]);
+               i++;
+               j++;
+           }
+           else if(nums1[i] < nums2[j]) i++;
+           else j++;
+       }
+       
+       return result;
+   }
+};
+
+int main(){
+    vector<int>nums1 = {1,2,2,1};
+    vector<int>nums2 =  {2,2};
+
+    vector<int> ret;
+    
+    Solution Example;
+    
+    ret = Example.intersect(nums1, nums2);
+    
+      printf("[");
+         
+         for(int i=0; i<ret.size(); i++){
+             printf("%i", ret[i]);
+         }
+          printf("]");
+}
+
+
+
+

c62ec5d6a5b5bdd7a413b107bdced21faf7bd438
nisunalade <aladenisun@yahoo.com>
2021-01-10 16:24:18 -0500

    new changes

--- LeetCode/pairs.cpp
+++ LeetCode/pairs.cpp
@@ -1,11 +1,56 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<document type="com.apple.InterfaceBuilder3.Cocoa.XIB" version="3.0" toolsVersion="11134" targetRuntime="MacOSX.Cocoa" propertyAccessControl="none" useAutolayout="YES" customObjectInstantitationMethod="direct">
-    <dependencies>
-        <plugIn identifier="com.apple.InterfaceBuilder.CocoaPlugin" version="11134"/>
-    </dependencies>
-    <objects>
-        <customObject id="-2" userLabel="File's Owner"/>
-        <customObject id="-1" userLabel="First Responder" customClass="FirstResponder"/>
-        <customObject id="-3" userLabel="Application" customClass="NSObject"/>
-    </objects>
-</document>
+/*
+    10 20 20 10 10 30 50 10 20
+    
+    SET:
+    10 : 4
+    20 : 3
+    30 : 1
+    50 : 1
+    
+    int pair: 3
+    
+    PSEUDOCODE:
+    - map <pair<int, int> PairCount;
+    - map <pair<int, int>::iterator itr;
+    - int pairs = 0;
+ 
+ PUT IN VALUES INTO MAP
+    for i in ar.size:
+        itr = map.find(ar[i])
+        if itr: increment second by 1
+        else: add ar[i] and set second to 1
+        
+OBTAIN THE COUNT
+    for itr in map:
+        pairs = pairs + (itr->second/2);
+    */
+
+//CREATE EMPTY MAP
+map <int , int> pairCount;
+map <int, int>::iterator itr;
+int pairs = 0;
+
+//BASE CASE
+if (ar.size() == 0) return 0;
+
+ //PUT IN VALUES INTO MAP
+for(int i=0; i<ar.size(); i++){
+    itr = pairCount.find(ar[i]);
+    
+    if(itr != pairCount.end()) itr->second++;
+    else {
+        pairCount.insert(pair<int, int>(ar[i], 1));
+    }
+}
+
+// for(itr = pairCount.begin(); itr != pairCount.end(); itr++){
+//     cout<<itr->first<<" "<<itr->second<<endl;
+// }
+
+//OBTAIN THE COUNT
+for(itr = pairCount.begin(); itr != pairCount.end(); itr++){
+    pairs += (itr->second)/2;
+}
+
+// cout<<"pairs: "<<pairs<<endl;
+return pairs;
